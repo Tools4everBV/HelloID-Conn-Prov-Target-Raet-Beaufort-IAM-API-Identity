efc98acf3ba5f44992a1c4e2f1c36161ecd5bcc5
rschouten97 <r.schouten@tools4ever.com>
2021-03-30 20:47:12 +0200

    WORK IN PROGRESS: new logging and extra checks

--- README.md
+++ README.md
@@ -1,44 +1,52 @@
-# HelloID-Conn-Prov-Target-Raet-Users
+# **Work-In-Progress - Untested**
+
+### HelloID-Conn-Prov-Target-Raet-Users
 This connector is able to write back the identity of a provisioned user (to another target like Azure AD or MS AD) to the user of Raet Beaufort. This field can be used in Beaufort for, by example, Single Sign-On purposes. Please keep in mind that for now, only the AccountCreate or AccountUpdate is triggering the possible change of the identity. (Disable, Delete and Enable are not neccesarry)
 
 Also keep in mind that this endpoint will be migrated to the new IAM-API later on.
 
-# Configuration
+### Configuration
 Please make your provisioned system dependant on this Users Target Connector and make sure that the values needed to be written back are stored on the account data (e.g UserPrincipalName).
 
-# Custom Connector Configuration
+### Custom Connector Configuration
 Please add the following JSON Custom Connector Configuration to this Target System and fill the Configuration Tab with the correct values of the Beaufort Environment.
 
 [
   {
-    "key": "clientid",
+    "key": "connection.clientId",
     "type": "input",
     "defaultValue": "",
     "templateOptions": {
-      "label": "Raet ClientID",
+      "label": "Client Id",
+      "placeholder": "Enter the client Id here",
+      "description": "",
       "required": true
     }
-    },
-    {
-    "key": "clientsecret",
+  },
+  {
+    "key": "connection.clientSecret",
     "type": "input",
     "defaultValue": "",
     "templateOptions": {
-      "label": "Raet Client Secret",
+      "label": "Client Secret",
+      "type": "password",
+      "placeholder": "Enter the Client Secret here",
       "required": true
     }
-    },
-    {
-    "key": "tenantid",
+  },
+  {
+    "key": "connection.tenantId",
     "type": "input",
     "defaultValue": "",
     "templateOptions": {
-      "label": "Raet TenantId",
-      "required": true
+      "label": "Tenant Id",
+      "placeholder": "Enter the Tenant Id here",
+      "description": "Optional. Not required in most implementations.",
+      "required": false
     }
-    }
-}
+  }
+]
 
-# More information
+### More information
 More information about the Users endpoint of the Raet Users Endpoint can be found on:
 https://community.raet.com/developers-community/w/iam-api/2695/iam-endpoints-users
---
+++ update.ps1
@@ -0,0 +1,168 @@
+$dryRun = $false
+
+$config = ConvertFrom-Json $configuration
+
+$clientId = $config.connection.clientId
+$clientSecret = $config.connection.clientSecret
+$tenantId = $config.connection.tenantId
+
+#Initialize default properties
+$p = $person | ConvertFrom-Json;
+$m = $manager | ConvertFrom-Json;
+$aRef = $accountReference | ConvertFrom-Json;
+$mRef = $managerAccountReference | ConvertFrom-Json;
+$success = $False;
+$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+#Change mapping here
+$account = [PSCustomObject]@{
+    displayName     = $p.DisplayName;
+    externalId      = $p.externalID;    
+    currentIdentity = $aref
+    identity        = $p.Accounts.ActiveDirectory.userPrincipalName;    
+}
+
+function New-RaetSession { 
+    [CmdletBinding()]
+    param (
+        [Alias("Param1")] 
+        [parameter(Mandatory = $true)]  
+        [string]      
+        $ClientId,
+
+        [Alias("Param2")] 
+        [parameter(Mandatory = $true)]  
+        [string]
+        $ClientSecret,
+
+        [Alias("Param3")] 
+        [parameter(Mandatory = $false)]  
+        [string]
+        $TenantId
+    )
+   
+    #Check if the current token is still valid
+    if (Confirm-AccessTokenIsValid -eq $true) {       
+        return
+    }
+
+    $url = "https://api.raet.com/authentication/token"
+    $authorisationBody = @{
+        'grant_type'    = "client_credentials"
+        'client_id'     = $ClientId
+        'client_secret' = $ClientSecret
+    } 
+    try {
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+        $result = Invoke-WebRequest -Uri $url -Method Post -Body $authorisationBody -ContentType 'application/x-www-form-urlencoded' -Headers @{'Cache-Control' = "no-cache" } -Proxy:$Proxy -UseBasicParsing
+        $accessToken = $result.Content | ConvertFrom-Json
+        $Script:expirationTimeAccessToken = (Get-Date).AddSeconds($accessToken.expires_in)
+
+        $Script:AuthenticationHeaders = @{
+            'X-Client-Id'      = $ClientId;
+            'Authorization'    = "Bearer $($accessToken.access_token)";
+            'X-Raet-Tenant-Id' = $TenantId;
+           
+        }     
+    }
+    catch {
+        if ($_.Exception.Response.StatusCode -eq "Forbidden") {
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
+        } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'" 
+        } else {
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'" 
+        }  
+        throw $errorMessage
+    } 
+}
+
+function Confirm-AccessTokenIsValid {
+    if ($null -ne $Script:expirationTimeAccessToken) {
+        if ((Get-Date) -le $Script:expirationTimeAccessToken) {
+            return $true
+        }        
+    }
+    return $false    
+}
+
+try{
+    If (($null -ne $account.identity) -AND ($account.identity -ne $account.Currentidentity)) {
+        $accessTokenValid = Confirm-AccessTokenIsValid
+        if ($accessTokenValid -ne $true) {
+            New-RaetSession -ClientId $clientId -ClientSecret $clientSecret -TenantId $TenantId
+        }
+
+        $getUrl = "https://api.raet.com/iam/v1.0/users(employeeId=$($account.externalID))"       
+        $getResult = Invoke-WebRequest -Uri $getUrl -Method GET -Headers $Script:AuthenticationHeaders -ContentType "application/json"
+
+        $raetCurrentIdentity = ($getResult.content | ConvertFrom-Json).identityId            
+        
+        if (($null -ne $raetCurrentIdentity) -AND ($account.identity -ne $raetCurrentIdentity)) {
+    
+            $userIdentity = [PSCustomObject]@{
+                id = $account.identity
+            }
+            $identityBody = $userIdentity | ConvertTo-Json        
+
+            $PatchUrl = "https://api.raet.com/iam/v1.0/users(employeeId=$($account.externalID))/identity"
+
+            if (-Not($dryRun -eq $True)) {            
+                $null = Invoke-WebRequest -Uri $PatchUrl -Method PATCH -Headers $Script:AuthenticationHeaders -ContentType "application/json" -Body $identityBody
+            }
+
+            $auditLogs.Add([PSCustomObject]@{
+                Action = "UpdateAccount"
+                Message = "Updated RAET user identity $($aRef)"
+                IsError = $false;
+            });
+    
+            $success = $true;
+        }
+        else {
+            $auditLogs.Add([PSCustomObject]@{
+                Action = "UpdateAccount"
+                Message = "Skipped update of RAET user identity $($aRef); ID empty or equal in RAET"
+                IsError = $false;
+            });
+    
+            $success = $true;     
+        }
+    }else{
+        $auditLogs.Add([PSCustomObject]@{
+            Action = "UpdateAccount"
+            Message = "Skipped update of RAET user identity $($aRef); ID empty or equal in HelloID"
+            IsError = $false;
+        });
+    
+        $success = $true; 
+    } 
+}catch{
+    $auditLogs.Add([PSCustomObject]@{
+        Action = "CreateAccount"
+        Message = "Error updating RAET user identity $($aRef): $($_)"
+        IsError = $True
+    });
+    Write-Error $_;    
+}
+
+
+# Send results
+$result = [PSCustomObject]@{
+	Success= $success;
+	AccountReference= $account.externalID;
+	AuditLogs = $auditLogs;
+    Account = $account;
+    #PreviousAccount = $previousAccount;    
+
+    # Optionally return data for use in other systems
+    ExportData       = [PSCustomObject]@{
+        displayName = $account.DisplayName;
+        identity    = $account.identity;
+        externalId  = $account.externalId;
+    };
+};
+Write-Output $result | ConvertTo-Json -Depth 10;
